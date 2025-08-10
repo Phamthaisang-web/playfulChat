b680fe288dfc247b3b7c3f46262aa2945b6cf137
Phạm Thái Sang <phamthaisang1710@gmail.com>
2025-08-10 22:54:29 +0700

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://playfulchat.onrender.com/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:1000/api" : "/api",
   withCredentials: true,
 });
