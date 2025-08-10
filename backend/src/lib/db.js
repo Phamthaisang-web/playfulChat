77dde1a6a0c399e42d284b42a7713de68f79e4c4
Phamthaisang <phamthaisang1710@gmail.com>
2025-08-10 22:39:09 +0700

    Initial commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect("mongodb+srv://phamthaisang1710:EdQh1SZR0N5X1O5Z@cluster0.upuajfm.mongodb.net/");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
